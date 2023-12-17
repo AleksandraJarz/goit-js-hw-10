0e93df6ce7cafaa5f5629200f9cba5d3a29022eb
AleksandraJarz <jarzebska.aleksandraa@gmail.com>
2023-12-17 23:23:26 +0100

    feat: commit

--- src/cat-api.js
+++ src/cat-api.js
@@ -7,7 +7,7 @@ export const fetchBreeds = () => {
   return axios.get('https://api.thecatapi.com/v1/breeds');
 };
 
-export const fetchCatByBreeds = breedId => {
+export const fetchCatByBreed = breedId => {
   return axios.get(
     'https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}'
   );
--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
+import { fetchBreed, fetchCatByBreed } from './cat-api';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_m2lgK5Nia1pd8DUl24UNtOhGRwIFqfFsx7kE9eRmryfEzHhp0uvjNBrqx2VKChqY';
+
+const breedSelect = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
+const catInfo = document.querySelector('cat-info');

736205636d5cfe6c6ae91a21603e938526e33ff4
AleksandraJarz <jarzebska.aleksandraa@gmail.com>
2023-12-17 23:51:22 +0100

    feat: update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-import axios from 'axios';
 import Notiflix from 'notiflix';
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
-
-axios.defaults.headers.common['x-api-key'] =
-  'live_m2lgK5Nia1pd8DUl24UNtOhGRwIFqfFsx7kE9eRmryfEzHhp0uvjNBrqx2VKChqY';
 
 const breedSelect = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
-const catInfo = document.querySelector('cat-info');
+const catInfo = document.querySelector('.cat-info');
 
 error.style.display = 'none';
 

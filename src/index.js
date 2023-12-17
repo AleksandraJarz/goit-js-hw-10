8e62338804bcdf1142876b566df58c31f2cea346
AleksandraJarz <jarzebska.aleksandraa@gmail.com>
2023-12-17 23:31:04 +0100

    feat: update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
-import { fetchBreed, fetchCatByBreed } from './cat-api';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 axios.defaults.headers.common['x-api-key'] =
   'live_m2lgK5Nia1pd8DUl24UNtOhGRwIFqfFsx7kE9eRmryfEzHhp0uvjNBrqx2VKChqY';
@@ -9,3 +9,18 @@ const breedSelect = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
 const catInfo = document.querySelector('cat-info');
+
+error.style.display = 'none';
+
+// lista ras
+breeds.style.display = 'none';
+fetchBreeds()
+  .then(data => {
+    const html = data.map(
+      breed => 'option value= "${breed.id}">"${breed.name}"</option>'
+    );
+    breeds.innerHTML = html;
+    breeds.style.display = 'block';
+    loader.style.display = 'none';
+  })
+  .catch(errpr => Notiflix.Notify.failure('Error loading breeds'));

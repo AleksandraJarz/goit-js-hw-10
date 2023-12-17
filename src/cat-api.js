1c3891630df3bf097d90629907aa984631f0d9f1
AleksandraJarz <jarzebska.aleksandraa@gmail.com>
2023-12-17 23:55:51 +0100

    feat: update

--- src/cat-api.js
+++ src/cat-api.js
@@ -3,12 +3,15 @@ import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
   'live_m2lgK5Nia1pd8DUl24UNtOhGRwIFqfFsx7kE9eRmryfEzHhp0uvjNBrqx2VKChqY';
 
-export const fetchBreeds = () => {
-  return axios.get('https://api.thecatapi.com/v1/breeds');
-};
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
 
-export const fetchCatByBreed = breedId => {
-  return axios.get(
-    'https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}'
-  );
-};
+export function fetchBreeds() {
+  return axions
+    .get('/breeds')
+    .then(({ data }) => data)
+    .catch(error => {
+      throw new Error();
+    });
+}
+
+export function fetchCatByBreed(breedId) {}
